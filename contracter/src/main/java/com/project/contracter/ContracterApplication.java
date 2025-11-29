979c74c0698dd82b98ec7c644556cfd5c937e14b
youssefgharbi1 <150081674+youssefgharbi1@users.noreply.github.com>
2025-11-29 22:35:03 +0100

    Initial project setup

---
+++ contracter/src/main/java/com/project/contracter/ContracterApplication.java
@@ -0,0 +1,13 @@
+package com.project.contracter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContracterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContracterApplication.class, args);
+	}
+
+}
